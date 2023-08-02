63ab6a71585d2594ad983ef3703d17addf0d618d
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-08-01 22:22:13 -0400

    Se crea el clase repositorio

---
+++ app/src/main/java/cl/awakelab/retrofitexample/data/Repositorio.kt
@@ -0,0 +1,12 @@
+package cl.awakelab.retrofitexample.data
+
+import cl.awakelab.retrofitexample.data.remote.Terreno
+import cl.awakelab.retrofitexample.data.remote.TerrenoAPI
+
+class Repositorio(private val terrenoAPI: TerrenoAPI) {
+
+   // suspend fun cargarTerreno(): List<Terreno>{
+
+      //  return terrenoAPI.getData()
+    //}
+}
